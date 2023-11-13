930f92d44b41dfbeac27c8e59fff291206ed8703
SIMI-ENOCH <enoch@systemspecscomng.onmicrosoft.com>
2023-11-13 01:10:23 +0100

    Feat: Project editted successfully

--- src/components/PortData.js
+++ src/components/PortData.js
@@ -7,23 +7,23 @@ const PortData = [
     id: 1,
     img: `${ecommerce}`,
     link: 'https://my-app-khaki-three.vercel.app',
-    p: 'Carsons is an innovative project that aims to revolutionize a seamless and personalized experience for customers, with the vision of transforming the e-commerce industry. It was built using Html, Css, React, Javascript',
+    p: 'Carsons is an innovative and ongoing project that aims to revolutionize a seamless and personalized experience for customers, with the vision of transforming the e-commerce industry. This project is still in progress as i am planning on implementing NodeJs for the backend and to handle some other technologies to handle payments and a good customer experience. Carsons was built using Reactjs and TailwindCSS',
     h4: 'Carsons, An e-commerce Website',
   },
   {
     id: 2,
     img: `${airbnb}`,
     link: 'https://airbnb-clone-beta-ten.vercel.app/',
-    p: 'In this project, i have created an Airbnb clone using React. We prioritized responsive design, robust listings, interactive maps, and most of the components and its functionalities functionalites working, and also some of its components with reviews and ratings. This journey continues with ongoing improvements and expansion, ensuring a top-notch user experience.',
+    p: 'In this project, i have created an Airbnb clone using React. I prioritized responsive design, robust listings, interactive maps, and most of the components and its functionalities functionalites working, and also some of its components with reviews and ratings. This journey continues with ongoing improvements and expansion, ensuring a top-notch user experience.',
     h4: 'Airbnb Clone - Zoom out for better view of the map',
   },
-  {
-    id: 3,
-    img: `${restaurant}`,
-    link: 'https://react-project-4-six.vercel.app',
-    p: 'This is a mini project that comprises of list of food items to improve sales, establishing a strong foundation for success in the food and hospitality industry ',
-    h4: 'A Restaurant Menu-Listing',
-  },
+  // {
+  //   id: 3,
+  //   img: `${restaurant}`,
+  //   link: 'https://react-project-4-six.vercel.app',
+  //   p: 'This is a mini project that comprises of list of food items to improve sales, establishing a strong foundation for success in the food and hospitality industry ',
+  //   h4: 'A Restaurant Menu-Listing',
+  // },
 ];
 
 export default PortData;
